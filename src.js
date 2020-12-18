4b6fb3e6c04f9e121af312b699f73e8d636b9782
rahulshahare <rahulshahare@gmail.com>
2020-12-18 14:30:07 +0530

    js is saparated

---
+++ src.js
@@ -0,0 +1,15 @@
+      class Calculator extends React.Component{
+          render(){
+              return(
+                  <div>
+                      <h1>Hello, Worlds</h1>
+                  </div>
+              )
+          }
+      }
+      ReactDOM.render(
+        <Calculator/>,
+        document.getElementById('root')
+      );
+
+  
